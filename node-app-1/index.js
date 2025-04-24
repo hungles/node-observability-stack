8da092c980fb83e7cdc08853d0ad01efc2c507a5
Sebastian Carmona <scarmona04@hotmail.com>
2025-04-23 22:40:33 -0500

    Add commentaries in index.js app

--- node-app-1/index.js
+++ node-app-1/index.js
@@ -1,30 +1,33 @@
+// Import required modules
 const express = require('express');
 const client = require('prom-client');
 
+// Create an Express application
 const app = express();
 const port = 3000;
 
-// Habilita las métricas estándar del proceso Node.js
+// Collect default metrics in second process
 client.collectDefaultMetrics();
 
-// Crear una métrica personalizada: contador de visitas
+// Create a custom metric: visit counter
 const visitCounter = new client.Counter({
   name: 'app_visits_total',
   help: 'Número total de visitas a la página principal',
 });
 
-// Ruta principal
+// Root path
 app.get('/', (req, res) => {
   visitCounter.inc(); // Incrementar el contador
   res.send('Hello, Prometheus! from Node-app-1');
 });
 
-// Endpoint para exponer métricas
+// Endpoint to expose metrics
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', client.register.contentType);
   res.end(await client.register.metrics());
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`App corriendo en http://localhost:${port}`);
+  console.log(`App running in http://localhost:${port}`);
 });
